43199c24d2f146131a4eff3efe887339d0444c45
Mohamed Ahmed <maxamednajiib10@hotmail.com>
2019-05-30 14:34:12 -0600

    adding my project to git

---
+++ app/src/main/java/com/najiibahmed/fadezonewaitlist/MainActivity.java
@@ -0,0 +1,76 @@
+package com.najiibahmed.fadezonewaitlist;
+
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+import android.support.v7.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
+    private NavigationView navigationView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        toolbar = findViewById(R.id.tool_bar);
+        setSupportActionBar(toolbar);
+
+        final ActionBar actionBar = getSupportActionBar();
+        actionBar.setDisplayHomeAsUpEnabled(true);
+        actionBar.setHomeAsUpIndicator(R.drawable.ic_menu);
+
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.navigationViewMainActivity);
+        navigationView.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+
+                switch (menuItem.getItemId()){
+                    case R.id.itemMenuProfile:
+                        displayMessage("Profile selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+                    case R.id.itemMenuLogout:
+                        displayMessage("Logout selected...");
+                        drawerLayout.closeDrawers();
+                        return true;
+                }
+
+
+                return false;
+            }
+        });
+
+    }
+
+    public void OnClick_AddNewCustomer(View view) {
+        // handles clicks on the add button
+    }
+
+    private void displayMessage(String message){
+        Toast.makeText(this, message,Toast.LENGTH_SHORT).show();
+
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
